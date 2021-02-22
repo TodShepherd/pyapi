086d5b2ed07927c62a40c066a3824590519d1afc
Tod Shepherd <tod.shepherd@t-mobile.com>
2021-02-22 20:51:28 +0000

    learning about lists

---
+++ hero.py
@@ -0,0 +1,12 @@
+
+fruitlist= ["strawberries",21,"orange",["corn","broccoli"],"grapes"]
+
+heroes= {"hulk": ["most dangerous","dumbest"],"spiderman": "least dangerous, pretty sharp","blackwidow": {"danger_level": "kinda dangerous", "intellect": "smartest"}}
+
+dang= ["strawberries",21,"orange",["corn","broccoli"],{"hulk": ["most dangerous","dumbest"],"spiderman": "least dangerous, pretty sharp","blackwidow": {"danger_level": "kinda dangerous", "intellect": "smartest"}}]
+
+
+print("Hello")
+print(fruitlist)
+print(dang[4]['blackwidow']['danger_level'])
+
